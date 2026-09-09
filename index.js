60b584feabf66e65e14526ae42726267ecf80812
shayanhuda0702 <shayanhuda0702@gmail.com>
2026-09-09 12:29:34 -0400

    Create Dark mode them for page

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-// template_bv6agsy
-// service_134dpnj
-// Public Key NFocqCVg3F7J7pOom
+let isModalOpen = false;
+let contrastToggle = false
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
     event.preventDefault();
@@ -24,7 +33,6 @@ function contact(event) {
     })  
 }
 
-let isModalOpen = false;
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
